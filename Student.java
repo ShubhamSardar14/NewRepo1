5e02148efbf9e6d9832ad1c0f5d53d2846101552
ShubhamSardar14 <87054843+ShubhamSardar14@users.noreply.github.com>
2022-03-16 10:24:07 +0530

    code

---
+++ Student.java
@@ -0,0 +1,9 @@
+class Student{
+
+public static void main(String[] arg){
+System.out.println("Hello there");
+System.out.println("Name- Shubham Sardar");
+System.out.println("Batch - B");
+System.out.println("CDAC NOIDA");
+}
+}
