69891b6771085230e4b20084a2342560e444811b
ShubhamSardar14 <87054843+ShubhamSardar14@users.noreply.github.com>
2022-03-16 10:26:33 +0530

    code for multiplication

---
+++ Multi.java
@@ -0,0 +1,13 @@
+class Multi{
+
+ static void mult(int n1,int n2,int n3){
+int mul=n1*n2*n3;
+System.out.println("Multiplication is ="+mul);
+}
+public static void main(String arg[]){
+	
+	mult(5,4,5);
+
+}
+
+}
